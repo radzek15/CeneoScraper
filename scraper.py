1b13cc763bfc2f96cd8627e12df165a5e10f1c9d
radzek15 <radzek15@wp.pl>
2023-04-29 00:51:04 +0100

    scraper.py done

--- scraper.py
+++ scraper.py
@@ -4,13 +4,32 @@ import re
 import math
 
 
+def get_element_value(review, tag, attribute):
+    e = review.find(tag, class_=attribute)
+    return None if e is None or e.text is None else e.text.replace("\n", " ")
+
+
+def get_element_size(review, tag, attribute):
+    e = review.find_all(tag, class_=attribute)
+    return len(e.text) if e is not None else 0
+
+
+def get_review_data(review):
+    e = review.find_all("time")
+    return None if len(e) == 0 else e[0].get('datetime')
+
+
+def get_purchase_date(review):
+    e = review.find_all("time")
+    return None if len(e) < 2 else e[1].get('datetime')
+
+
 class Scraper:
 
-    def __int__(self, product_id):
+    def __init__(self, product_id):
         self.product_id = product_id
         self.product_url = requests.get(self.get_url())
         self.soup = BeautifulSoup(self.product_url.text, 'html.parser')
-
 
     def get_product_name(self):
         return get_element_value(self.soup, "h1", "product-top__product-info__name") or "No-name"
@@ -23,25 +42,42 @@ class Scraper:
         if not elements:
             raise Exception("Product with this ID does not exist!")
 
-        opinion_number = int(re.search(r'\d+', elements[2] if elements[2] else "0"))
-        reviews = []
-        pages_number = math.ceil(opinion_number / 10)
-
+        opinion_number = int(re.search(r'\d+', elements[2].text if
+                             re.search(r'\d+', elements[2].text) else "0").group())
         if opinion_number > 500:
             raise Exception("This Product has too many opinions, enter a different product id")
 
-def get_element_value(review, tag, attribute):
-    e = review.find(tag, class_=attribute)
-    return None if e is None or e.text is None else e.text.replace("\n", " ")
+        reviews = []
+        page, all_pages = 1, math.ceil(opinion_number / 10)
 
-def get_element_size(review, tag, attribute):
-    e = review.find_all(tag, class_=attribute)
-    return len(e.text) if e is not None else 0
+        while page <= all_pages:
+            soup = BeautifulSoup(requests.get(f'https://www.ceneo.pl/{self.product_id}'
+                                              f'/opinie-{str(page)}').text, 'html.parser')
+            reviews.extend(self.remove_empty(soup.find_all("div", class_="user-post__body")))
+            page += 1
 
-def get_review_data(review):
-    e = review.find_all("time")
-    return None if len(e) == 0 else e[0].get('datetime')
+        return reviews
 
-def get_purhase_date(review):
-    e = review.find_all("time")
-    return None if len(e) < 2 else e[1].get('datetime')
+    def remove_empty(self, reviews):
+        return [self.get_details(i) for i in reviews if self.get_details(i)['author'] or self.get_details(i)['score']]
+
+    @staticmethod
+    def get_details(review):
+        ft = review.find_all("div", class_="review-feature__col")
+        details = {"author": get_element_value(review, "span", "user-post__author-name"),
+                   "comment_date": get_review_data(review),
+                   "purchase_date": (get_purchase_date(review) or "0"),
+                   "opinion": get_element_value(review, "div", "user-post__text"),
+                   "recommendation": (get_element_value(review, "em", "recommended") or "Nie polecam"),
+                   "score": get_element_value(review, "span", "user-post__score-count"),
+                   "likes": review.find("button", class_="js_vote-yes").find("span").text if review.find(
+                       "button", class_="js_vote-yes") else '0',
+                   "dislikes": review.find("button", class_="js_vote-no").find("span").text if review.find(
+                       "button", class_="js_vote-no") else '0',
+                   'pros': [p.text for p in ft[0].findChildren("div", recursive=False)
+                            if 'review-feature__item' in p['class']] if len(ft) > 0 else '0',
+                   'cons': [c.text for c in ft[1].findChildren("div", recursive=False)
+                            if 'review-feature__item' in c['class']] if len(ft) > 1 else '0'
+                   }
+
+        return details
