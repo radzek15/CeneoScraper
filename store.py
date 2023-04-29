c25aa2b5fbee2bc608cda3e2e092d60dd425eaa0
radzek15 <radzek15@wp.pl>
2023-04-29 22:25:05 +0100

    added store.py

---
+++ store.py
@@ -0,0 +1,52 @@
+from statistics import mean
+
+
+class Store:
+    def __init__(self):
+        self.store = {}
+
+    def add_product(self, p_id, name, opinions):
+        self.store[p_id] = {"ID": p_id, "name": name, "opinions": opinions, 'stats': {}}
+
+    def get_product(self, p_id):
+        return self.store[p_id]
+
+    def get_product_name(self, p_id):
+        return self.store[p_id]["name"]
+
+    def get_all_products(self):
+        return [i for i in self.store.values()]
+
+    def get_all_opinions(self):
+        return [i['opinions'] for i in self.store.values()]
+
+    def get_product_opinions(self, p_id):
+        return self.store[p_id]["opinions"]
+
+    def set_stats(self, p_id, stats):
+        self.store[p_id]["stats"] = stats
+
+    def get_stats(self, p_id):
+        return self.store[p_id]["stats"]
+
+# static methods
+    @staticmethod
+    def calculate_recommendations(reviews):
+        return {'positive': sum([1 for i in reviews if i['recommendation'] == 'Polecam']),
+                'negative': sum([1 for i in reviews if i['recommendation'] == 'Nie polecam'])}
+
+    @staticmethod
+    def calculate_ratings(reviews):
+        scores = {'0,5': 0, '1': 0, '1,5': 0, '2': 0, '2,5': 0, '3': 0, '3,5': 0, '4': 0, '4,5': 0, '5': 0}
+        for review in reviews:
+            scores[review['score'].split('/')[0]] += 1
+        return scores
+
+    @staticmethod
+    def calculate_stats(reviews):
+        stats = {'opinion_number': len(reviews),
+                 'props_number': sum([review['props'] for review in reviews]),
+                 'cons_number': sum([review['cons'] for review in reviews]),
+                 'mean': round(mean([float(i['score'].split('/').replace(',', '.')) for i in reviews]), 2)
+                 }
+        return stats
