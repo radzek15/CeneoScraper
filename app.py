f40dec545d60bb18a10171d368a8254267a3e960
radzek15 <radzek15@wp.pl>
2023-05-15 14:20:03 +0100

    .html > .jinja

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app_store = Store()
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.jinja')
 
 
 @app.route('/extract', methods=['GET'])
 def _product():
     product_id = request.args.get('product_id')
     if product_id is None:
-        return render_template('extract.html')
+        return render_template('extract.jinja')
     try:
         scrap = Scraper(product_id)
         app_store.add_product(product_id, scrap.get_product_name(), scrap.get_all_opinions())
@@ -27,29 +27,29 @@ def _product():
             jsonpickle.set_encoder_options('json', indent=2)
             f.write(jsonpickle.encode(scrap.get_all_opinions()))
 
-        return render_template('product.html', reviews=scrap.get_all_opinions(),
+        return render_template('product.jinja', reviews=scrap.get_all_opinions(),
                                product_id=product_id, json=jsonpickle.encode(scrap.get_all_opinions()),
                                reviews_number=len(scrap.get_all_opinions()))
     except Exception as e:
         print("Error: ", e)
-        return render_template('extract.html', error=str(e))
+        return render_template('extract.jinja', error=str(e))
 
 
 @app.route(rule='/product', methods=['GET'])
 def opinion():
-    return render_template('product.html', error="Wrong product ID!")
+    return render_template('product.jinja', error="Wrong product ID!")
 
 
 @app.route(rule='/details/<product_id>', methods=['GET'])
 def details(product_id):
     reviews = app_store.get_product_opinions(product_id)
-    return render_template('details.html', id=product_id, product_data=app_store.calculate_recommendations(reviews),
+    return render_template('details.jinja', id=product_id, product_data=app_store.calculate_recommendations(reviews),
                            rating=app_store.calculate_ratings(reviews), product_name=app_store.get_product_name(product_id))
 
 
 @app.route(rule='/all_products', methods=['GET'])
 def product_list():
-    return render_template('all_products.html', products=app_store.get_all_products())
+    return render_template('all_products.jinja', products=app_store.get_all_products())
 
 
 if __name__ == '__main__':
